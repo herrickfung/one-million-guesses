9961d205006e3efcd3f67d6f12b05ed876f51ff4
Herrick Fung <herrickfung@gmail.com>
2020-02-19 22:12:27 +0800

    'middle_way_mod'

---
+++ round_down_middle_way.py
@@ -0,0 +1,51 @@
+# This stimulation will run 1 million guesses by rounding down the average between upper and lower bound.
+
+import random
+import csv
+import math
+
+logfile = open("round_down_middle_way.csv", 'w', newline = '')
+writer = csv.writer(logfile, delimiter = ',')
+writer.writerow(['trial', 'No_of_Guesses', 'Answer', 'GuessList'])
+
+list = []
+
+for i in range(100):
+	list.append(i + 1)
+
+
+def game():
+	answer = random.choice(list)
+	guesslist = []
+
+	lower = list[0]
+	upper = list[-1]
+
+	guess = math.floor((lower + upper) / 2)
+	guesslist.append(guess)
+
+	for j in range(100):
+
+		if guess > answer:
+			upper = guess
+			guess = math.floor((lower + upper) / 2)
+			guesslist.append(guess)
+			j += 1
+
+		elif guess < answer:
+			lower = guess
+			guess = math.floor((lower + upper) / 2)
+			guesslist.append(guess)
+			j += 1
+
+		else:
+			save = len(guesslist) 
+			writer.writerow([(k + 1), save, answer, guesslist])
+			break
+
+
+if __name__ == '__main__':
+	for k in range(1000000):
+		game()
+		k += 1
+	logfile.close()
--- round_up_middle_way.py
+++ round_up_middle_way.py
@@ -1,50 +1,51 @@
-# This stimulation will run 1 million guesses by rounding off the average between upper and lower bound.
+# This stimulation will run 1 million guesses by rounding up the average between upper and lower bound.
 
 import random
 import csv
+import math
 
-logfile = open("middle_way.csv", 'w', newline = '')
+logfile = open("round_up_middle_way.csv", 'w', newline = '')
 writer = csv.writer(logfile, delimiter = ',')
 writer.writerow(['trial', 'No_of_Guesses', 'Answer', 'GuessList'])
 
 list = []
 
 for i in range(100):
-	list.append(i + 1)
+    list.append(i + 1)
 
 
 def game():
-	answer = random.choice(list)
-	guesslist = []
+    answer = random.choice(list)
+    guesslist = []
 
-	lower = list[0]
-	upper = list[-1]
+    lower = list[0]
+    upper = list[-1]
 
-	guess = round((lower + upper) / 2)
-	guesslist.append(guess)
+    guess = math.ceil((lower + upper) / 2)
+    guesslist.append(guess)
 
-	for j in range(100):
+    for j in range(100):
 
-		if guess > answer:
-			upper = guess
-			guess = round((lower + upper) / 2)
-			guesslist.append(guess)
-			j += 1
+        if guess > answer:
+            upper = guess
+            guess = math.ceil((lower + upper) / 2)
+            guesslist.append(guess)
+            j += 1
 
-		elif guess < answer:
-			lower = guess
-			guess = round((lower + upper) / 2)
-			guesslist.append(guess)
-			j += 1
+        elif guess < answer:
+            lower = guess
+            guess = math.ceil((lower + upper) / 2)
+            guesslist.append(guess)
+            j += 1
 
-		else:
-			save = len(guesslist) 
-			writer.writerow([(k + 1), save, answer, guesslist])
-			break
+        else:
+            save = len(guesslist)
+            writer.writerow([(k + 1), save, answer, guesslist])
+            break
 
 
 if __name__ == '__main__':
-	for k in range(1000000):
-		game()
-		k += 1
-	logfile.close()
+    for k in range(1000000):
+        game()
+        k += 1
+    logfile.close()
